8cdff0fb35d7cf8f6de8f4016fd5f3772afe8810
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-24 18:04:53 -0400

    feat: gradient loader added

---
+++ src/components/GradientLoader.jsx
@@ -0,0 +1,10 @@
+const GradientLoader = () => {
+  return (
+    <div className="relative inline-flex rounded-full p-1 overflow-hidden">
+      <div className="absolute -inset-[900%] animate-spin bg-[conic-gradient(from_90deg_at_30%_50%,#009cc4_0%,#00b8e6_80%,#ccffda_100%)] blur-lg"></div>
+      <span className="relative p-5 bg-slate-950 rounded-full"></span>
+    </div>
+  );
+};
+
+export default GradientLoader;
