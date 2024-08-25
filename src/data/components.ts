ff33d3303cc7218cd15a18a65aeb25fb6629ce65
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-25 17:11:14 -0400

    feat: components object added

---
+++ src/data/components.ts
@@ -0,0 +1,31 @@
+import GradientBorderButton from "../components/GradientBorderButton";
+import GradientHoverButton from "../components/GradientHoverButton";
+import AnimatedBackgroundButton from "../components/AnimatedBackgroundButton";
+import GradientLoader from "../components/GradientLoader";
+
+export const COMPONENTS = [
+  {
+    title: "Gradient border button",
+    component: GradientBorderButton,
+    page: "gradient-border-button",
+    path: "/src/components/GradientBorderButton.jsx",
+  },
+  {
+    title: "Gradient hover button",
+    component: GradientHoverButton,
+    page: "gradient-hover-button",
+    path: "/src/components/GradientHoverButton.jsx",
+  },
+  {
+    title: "Animated background button",
+    component: AnimatedBackgroundButton,
+    page: "animated-background-button",
+    path: "/src/components/AnimatedBackgroundButton.jsx",
+  },
+  {
+    title: "Gradient Loader",
+    component: GradientLoader,
+    page: "gradient-loader",
+    path: "/src/components/GradientLoader.jsx",
+  },
+];
