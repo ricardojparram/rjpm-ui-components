0899419feb11ef5e7b58783c4e4c6aa2141cc6a3
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-27 22:38:09 -0400

    feat: sitemap dependency added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://rjpm-ui.vercel.app/",
+  integrations: [tailwind(), react(), sitemap()],
 });
