20f2e03dcd14436342beefe9eb18642f253c1345
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-09-06 09:39:33 -0400

    feat: components made in react with his option in astro

--- src/components/GradientText.jsx
+++ src/components/GradientText.jsx
@@ -1,6 +1,6 @@
 const GradientText = ({ text = "Sample text" }) => {
   return (
-    <p class="bg-gradient-to-tr bg-[size:100%_200%] bg-[position:20%_0%] hover:bg-[position:90%_80%] transition-all duration-300 font-bold text-2xl from-blue-600 via-sky-400 to-indigo-600 bg-clip-text text-transparent z-10">
+    <p className="bg-gradient-to-tr bg-[size:100%_200%] bg-[position:20%_0%] hover:bg-[position:90%_80%] transition-all duration-300 font-bold text-2xl from-blue-600 via-sky-400 to-indigo-600 bg-clip-text text-transparent z-10">
       {text}
     </p>
   );
