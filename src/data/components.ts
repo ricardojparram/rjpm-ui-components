7f2f096c4ad983096ff737b56c38036c63d5c7b8
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-09-07 13:38:31 -0400

    feat: components dir in a constant

--- src/data/components.ts
+++ src/data/components.ts
@@ -2,27 +2,27 @@ import GradientBorderButton from "../components/GradientBorderButton.jsx";
 import GradientHoverButton from "../components/GradientHoverButton.jsx";
 import AnimatedBackgroundButton from "../components/AnimatedBackgroundButton.jsx";
 import GradientLoader from "../components/GradientLoader.jsx";
-import ThemeSwitch from "../components/ThemeSwitchPreview.astro";
+import ThemeSwitch from "../components/ThemeSwitch.astro";
 import GradientText from "../components/GradientText.jsx";
-import FlipCard from "../components/FlipCard.astro";
+import FlipCard from "../components/FlipCard.jsx";
 import InfiniteCarousel from "../components/InfiniteCarousel.astro";
 import { getFileContent } from "../util/getFileContent.js";
 
+const components_dir = "/src/components/";
 export const COMPONENTS = [
   {
     title: "Gradient border button",
     component: GradientBorderButton,
     page: "gradient-border-button",
-    filename: "GradientBorderButton.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/GradientBorderButton.jsx"),
+        code: await getFileContent(components_dir + "GradientBorderButton.jsx"),
       },
       {
         framework: "Astro",
         code: await getFileContent(
-          "/src/components/GradientBorderButton.astro"
+          components_dir + "GradientBorderButton.astro"
         ),
       },
     ],
@@ -31,15 +31,16 @@ export const COMPONENTS = [
     title: "Gradient hover button",
     component: GradientHoverButton,
     page: "gradient-hover-button",
-    filename: "GradientHoverButton.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/GradientHoverButton.jsx"),
+        code: await getFileContent(components_dir + "GradientHoverButton.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/GradientHoverButton.astro"),
+        code: await getFileContent(
+          components_dir + "GradientHoverButton.astro"
+        ),
       },
     ],
   },
@@ -47,18 +48,17 @@ export const COMPONENTS = [
     title: "Animated background button",
     component: AnimatedBackgroundButton,
     page: "animated-background-button",
-    filename: "AnimatedBackgroundButton.jsx",
     files: [
       {
         framework: "React",
         code: await getFileContent(
-          "/src/components/AnimatedBackgroundButton.jsx"
+          components_dir + "AnimatedBackgroundButton.jsx"
         ),
       },
       {
         framework: "Astro",
         code: await getFileContent(
-          "/src/components/AnimatedBackgroundButton.astro"
+          components_dir + "AnimatedBackgroundButton.astro"
         ),
       },
     ],
@@ -67,15 +67,14 @@ export const COMPONENTS = [
     title: "Gradient Loader",
     component: GradientLoader,
     page: "gradient-loader",
-    filename: "GradientLoader.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/GradientLoader.jsx"),
+        code: await getFileContent(components_dir + "GradientLoader.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/GradientLoader.astro"),
+        code: await getFileContent(components_dir + "GradientLoader.astro"),
       },
     ],
   },
@@ -83,15 +82,14 @@ export const COMPONENTS = [
     title: "Theme switch",
     component: ThemeSwitch,
     page: "theme-switch",
-    filename: "ThemeSwitch.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/ThemeSwitch.jsx"),
+        code: await getFileContent(components_dir + "ThemeSwitch.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/ThemeSwitch.astro"),
+        code: await getFileContent(components_dir + "ThemeSwitch.astro"),
       },
     ],
     tailwind: `export default {
@@ -104,15 +102,14 @@ export const COMPONENTS = [
     title: "Gradient text",
     component: GradientText,
     page: "gradient-text",
-    filename: "GradientText.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/GradientText.jsx"),
+        code: await getFileContent(components_dir + "GradientText.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/GradientText.astro"),
+        code: await getFileContent(components_dir + "GradientText.astro"),
       },
     ],
   },
@@ -120,15 +117,14 @@ export const COMPONENTS = [
     title: "Flip card",
     component: FlipCard,
     page: "flip-card",
-    filename: "FlipCard.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/FlipCard.jsx"),
+        code: await getFileContent(components_dir + "FlipCard.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/FlipCard.astro"),
+        code: await getFileContent(components_dir + "FlipCard.astro"),
       },
     ],
   },
@@ -136,15 +132,14 @@ export const COMPONENTS = [
     title: "Infinite carousel",
     component: InfiniteCarousel,
     page: "infinite-carousel",
-    filename: "InfiniteCarousel.jsx",
     files: [
       {
         framework: "React",
-        code: await getFileContent("/src/components/InfiniteCarousel.jsx"),
+        code: await getFileContent(components_dir + "InfiniteCarousel.jsx"),
       },
       {
         framework: "Astro",
-        code: await getFileContent("/src/components/InfiniteCarousel.astro"),
+        code: await getFileContent(components_dir + "InfiniteCarousel.astro"),
       },
     ],
     tailwind: `theme: {
